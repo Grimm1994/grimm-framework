72252aaf8fe491b76946d72b72f82dd0bafee4d8
grimm994 <grimm250894@gmail.com>
2022-10-16 02:25:51 +0300

    remove non-existing bullet from dummy view

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -31,9 +31,6 @@ use grimm1994\grimmCore\Application;
                     <a class="nav-link active" aria-current="page"
                        href="/">Home</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/contact">Contact</a>
-                </li>
             </ul>
             <?php if (Application::isGuest()): ?>
                 <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
